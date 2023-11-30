38afa08b03bc8e1037e894ae4eafc5cee4265f57
swaraj <ghoshswaraj2k@gmail.com>
2023-11-30 23:43:49 +0530

    added basic spring boot app

---
+++ playground/src/main/java/com/swaraj/projectx/PlayApp.java
@@ -0,0 +1,17 @@
+package com.swaraj.projectx;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayApp implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayApp.class, args);
+    }
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
