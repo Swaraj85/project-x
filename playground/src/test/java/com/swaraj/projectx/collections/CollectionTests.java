dc40ea883d1816a3800a5228eddf49549b94323b
swaraj <ghoshswaraj2k@gmail.com>
2023-12-01 09:20:58 +0530

    added basic tests

---
+++ playground/src/test/java/com/swaraj/projectx/collections/CollectionTests.java
@@ -0,0 +1,14 @@
+package com.swaraj.projectx.collections;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CollectionTests {
+
+    @Test
+    void testMe(){
+        assertEquals(1,1);
+    }
+}
