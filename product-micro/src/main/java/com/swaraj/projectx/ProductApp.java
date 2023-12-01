9f61803c7f2b429ab818c02e1afdb6b38ead53e4
swaraj <ghoshswaraj2k@gmail.com>
2023-12-01 19:18:38 +0530

    added connectivity with mysql

---
+++ product-micro/src/main/java/com/swaraj/projectx/ProductApp.java
@@ -0,0 +1,15 @@
+package com.swaraj.projectx;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class ProductApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApp.class, args);
+    }
+}
